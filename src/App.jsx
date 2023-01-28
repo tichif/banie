a38f53fa9a39e33fcac6aad9cbe3b3e47959845f
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-01-27 19:26:55 -0500

    Create gallery page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-// import Gallery from './pages/Gallery';
+import Gallery from './pages/Gallery';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
-        {/* <Route path='/gallery' element={<Gallery />} /> */}
+        <Route path='/gallery' element={<Gallery />} />
         <Route path='*' element={<Home />} />
       </Routes>
     </Router>
---
+++ src/pages/Gallery.jsx
@@ -0,0 +1,71 @@
+import Image1 from '../assets/images/1.jpg';
+import Image2 from '../assets/images/2.jpg';
+import Image3 from '../assets/images/3.jpg';
+import Image4 from '../assets/images/4.jpg';
+import Image5 from '../assets/images/5.jpg';
+import Image6 from '../assets/images/6.jpg';
+import Image7 from '../assets/images/7.jpg';
+import Image8 from '../assets/images/8.jpg';
+
+const Gallery = () => {
+  const photos = [
+    {
+      id: 1,
+      path: Image1,
+      alt: 'Banie',
+    },
+    {
+      id: 2,
+      path: Image2,
+      alt: 'Banie',
+    },
+    {
+      id: 3,
+      path: Image3,
+      alt: 'Banie',
+    },
+    {
+      id: 4,
+      path: Image4,
+      alt: 'Banie',
+    },
+    {
+      id: 5,
+      path: Image5,
+      alt: 'Banie',
+    },
+    {
+      id: 6,
+      path: Image6,
+      alt: 'Banie',
+    },
+    {
+      id: 7,
+      path: Image7,
+      alt: 'Banie',
+    },
+    {
+      id: 8,
+      path: Image8,
+      alt: 'Banie',
+    },
+  ];
+
+  return (
+    <>
+      <h2>Banie</h2>
+      <p className='gallery-text'>
+        Explaining my love for you is trying to explain color to a blind person.
+      </p>
+      <div className='gallery'>
+        {photos.map((photo) => (
+          <div key={photo.id}>
+            <img src={photo.path} alt={photo.alt} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Gallery;
